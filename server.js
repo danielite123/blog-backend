008bd411f0bd79963d88352a1aa5f5972042dbe1
danielite123 <danielite100@gmail.com>
2025-03-07 12:14:54 +0100

    first commit

--- server.js
+++ server.js
@@ -4,20 +4,58 @@ import "dotenv/config";
 import bcrypt from "bcryptjs";
 import { nanoid } from "nanoid";
 import jwt from "jsonwebtoken";
+import cors from "cors";
+import admin from "firebase-admin";
+// import serviceAccountKey from "./blog-project-77264-firebase-adminsdk-fbsvc-e63ce0f06a.json" assert { type: "json" };
+import serviceAccountKey from "./serviceAccountKey.js";
+import { v2 as cloudinary } from "cloudinary";
+import multer from "multer";
 
 import User from "./Schema/User.js";
+import Blog from "./Schema/Blog.js";
 
 const server = express();
 let PORT = 3000;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccountKey),
+});
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const getAuth = admin.auth;
 
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/; // regex for email
 let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; // regex for password
 
 server.use(express.json());
+server.use(cors());
 
 mongoose.connect(process.env.DB_LOCATION, {
   autoIndex: true,
 });
+
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) {
+    return res.status(401).json({ error: "No Access token" });
+  }
+  jwt.verify(token, process.env.SECRET_ACCESS_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Access token is invalid" });
+    }
+    req.user = user.id;
+    next();
+  });
+};
 
 const formatDatatoSend = (user) => {
   const access_token = jwt.sign(
@@ -95,12 +133,328 @@ server.post("/signin", (req, res) => {
 
   User.findOne({ "personal_info.email": email })
     .then((user) => {
-      console.log(user);
-      return res.json({ status: "got user document" });
+      if (!user) {
+        return res.status(403).json({ error: "Email not found" });
+      }
+
+      if (!user.google_auth) {
+        bcrypt.compare(password, user.personal_info.password, (err, result) => {
+          if (err) {
+            return res
+              .status(403)
+              .json({ error: "Error occured while login please try again" });
+          }
+          if (!result) {
+            return res.status(403).json({ error: "Incorrect password" });
+          } else {
+            return res.status(200).json(formatDatatoSend(user));
+          }
+        });
+      } else {
+        return res.status(403).json({
+          error:
+            "This email was signed up with Google. Please log in with Google to access the account",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/google-auth", async (req, res) => {
+  try {
+    const { access_token } = req.body;
+
+    if (!access_token) {
+      return res.status(400).json({ error: "Access token is missing" });
+    }
+
+    // Verify Firebase ID Token
+    const decodedUser = await admin.auth().verifyIdToken(access_token);
+
+    let { email, name, picture } = decodedUser;
+    picture = picture.replace("s96-c", "s384-c");
+
+    let user = await User.findOne({ "personal_info.email": email }).select(
+      "personal_info.fullname personal_info.username personal_info.profile_img google_auth"
+    );
+
+    if (user) {
+      if (!user.google_auth) {
+        return res.status(403).json({
+          error:
+            "This email was signed up without Google. Please log in with a password to access the account",
+        });
+      }
+    } else {
+      let username = await generateUsername(email);
+
+      user = new User({
+        personal_info: {
+          fullname: name,
+          email,
+          username,
+        },
+        google_auth: true,
+      });
+
+      await user.save();
+    }
+
+    return res.status(200).json(formatDatatoSend(user));
+  } catch (err) {
+    console.error("Google Auth Error:", err);
+    return res.status(500).json({
+      error: "Failed to authenticate with Google. Try another Google account.",
+    });
+  }
+});
+
+server.post("/upload", upload.single("image"), async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No file uploaded" });
+    }
+
+    const result = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream(
+          { folder: "uploads" }, // Optional folder name
+          (error, result) => {
+            if (error) reject(error);
+            else resolve(result);
+          }
+        )
+        .end(req.file.buffer);
+    });
+
+    return res.status(200).json({ imageUrl: result.secure_url });
+  } catch (err) {
+    console.error("Upload Error:", err);
+    return res.status(500).json({ error: "Image upload failed" });
+  }
+});
+
+server.post("/latest-blogs", (req, res) => {
+  let { page } = req.body;
+
+  let maxLimit = 5;
+
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/all-latest-blogs-count", (req, res) => {
+  Blog.countDocuments({ draft: false })
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count });
     })
     .catch((err) => {
       console.log(err);
-      return res.status(403).json({ error: "Email not found" });
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.get("/trending-blogs", (req, res) => {
+  Blog.find({ draft: false })
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({
+      "activity.total_reads": -1,
+      "activity.total_likes": -1,
+      publishedAt: -1,
+    })
+    .select("blog_id title publishedAt -_id")
+    .limit(5)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-blogs", (req, res) => {
+  let { tag, author, query, page } = req.body;
+
+  let findQuery;
+
+  if (tag) {
+    findQuery = { tags: tag, draft: false };
+  } else if (query) {
+    findQuery = { draft: false, title: new RegExp(query, "i") };
+  } else if (author) {
+    findQuery = { author, draft: false };
+  }
+
+  let maxLimit = 2;
+
+  Blog.find(findQuery)
+    .populate(
+      "author",
+      "personal_info.profile_img personal_info.username personal_info.fullname -_id"
+    )
+    .sort({ publishedAt: -1 })
+    .select("blog_id title des banner activity tags publishedAt -_id")
+    .skip((page - 1) * maxLimit)
+    .limit(maxLimit)
+    .then((blogs) => {
+      return res.status(200).json({ blogs });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-blogs-count", (req, res) => {
+  let { tag, author, query } = req.body;
+
+  let findQuery;
+
+  if (tag) {
+    findQuery = { tags: tag, draft: false };
+  } else if (query) {
+    findQuery = { draft: false, title: new RegExp(query, "i") };
+  } else if (author) {
+    findQuery = { author, draft: false };
+  }
+
+  Blog.countDocuments(findQuery)
+    .then((count) => {
+      return res.status(200).json({ totalDocs: count });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/search-users", (req, res) => {
+  let { query } = req.body;
+
+  User.find({
+    "personal_info.username": new RegExp(query, "i"),
+  })
+    .limit(50)
+    .select(
+      "personal_info.fullname personal_info.username personal_info.profile_img -_id"
+    )
+    .then((users) => {
+      return res.status(200).json({ users });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/get-profile", (req, res) => {
+  let { username } = req.body;
+
+  User.findOne({ "personal_info.username": username })
+    .select("-personal_info.password -google_auth -updateAt -blogs")
+    .then((user) => {
+      return res.status(200).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).json({ error: err.message });
+    });
+});
+
+server.post("/create-blog", verifyJWT, (req, res) => {
+  let authorId = req.user;
+
+  let { title, des, banner, tags, content, draft } = req.body;
+
+  if (!title.length) {
+    return res.status(403).json({ error: "You must provide a title" });
+  }
+
+  if (!draft) {
+    if (!des.length || des.length > 200) {
+      return res.status(403).json({
+        error: "You must provide blog description under 200 character",
+      });
+    }
+
+    if (!banner.length) {
+      return res
+        .status(403)
+        .json({ error: "You must provide blog banner to publish" });
+    }
+
+    if (!content.blocks.length) {
+      return res
+        .status(403)
+        .json({ error: "There must be some blog content to publish it" });
+    }
+
+    if (!tags.length || tags.length > 10) {
+      return res.status(403).json({
+        error: "Provide tags in order to publish the blog, Maximum 10",
+      });
+    }
+  }
+
+  tags = tags.map((tag) => tag.toLowerCase());
+
+  let blog_id =
+    title
+      .replace(/[^a-zA-Z0-9]/g, " ")
+      .replace(/\s+/g, "")
+      .trim() + nanoid();
+
+  let blog = new Blog({
+    title,
+    des,
+    banner,
+    content,
+    tags,
+    author: authorId,
+    blog_id,
+    draft: Boolean(draft),
+  });
+
+  blog
+    .save()
+    .then((blog) => {
+      let incrementVal = draft ? 0 : 1;
+
+      User.findOneAndUpdate(
+        { _id: authorId },
+        {
+          $inc: { "account_info.total_posts": incrementVal },
+          $push: { blogs: blog._id },
+        }
+      )
+        .then((user) => {
+          return res.status(200).json({ id: blog.blog_id });
+        })
+        .catch((err) => {
+          return res
+            .status(500)
+            .json({ error: "failed to update total post number" });
+        });
+    })
+    .catch((err) => {
+      return res.status(500).json({ error: err.message });
     });
 });
 
---
+++ serviceAccountKey.js
@@ -0,0 +1,16 @@
+export default {
+  type: "service_account",
+  project_id: "blog-project-77264",
+  private_key_id: "e63ce0f06a64fcd6038fed12c02765bde030d80a",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDnAAaPvVQYl6i9\naSsqv2DoopFb9Kljz2qIO4RJFIEydC0nw5WGhRWmbdo/AAv5kUrgAxgnS4ZkCY6f\ntk3atFlrSzydn8eAaewH/poMeIPcMQ62zYjtRnI/Bxg3inInAGIjiv+FkG8iU1Iu\n6aB3yXXoyFCQ5IDKXC1ov1UqN/xIYi0Mk3/d/+YArzayIdNKOIAH6pdxVy7RH+eh\nbPj17fbgdtPzjUuPs6tJz1JeP2y52+w+qfhmAvqvBKhAMzW95+m219+J+8+6yIw8\ny1wTybNdYfOdj8Bj6mMYVrCzMiICnUBFgVSvl+Rca2gbZtroCwh69V0OgMGFqNQF\niqcSnoDfAgMBAAECggEAaAoV6nVeQ/lCcZTFcWpirkRR7bWi+SY5GASeTv/iNKQo\nlkk7tiuIlb3SVx+hRRUQQRaDsWcNr06yovJL3F3wDCgFtAR7qTWLrgoYvdclpG2f\n7+tgaWWaHRMH8DP1t+UvTkzTSwfDVhVq5WAp+umMpnxZ9PhFnV6owQ2zwOEYSky6\nLbTRz2RCHTiZgw+zM33JhSO5XnEkqR3TMOTXprhoLyaSXa35gL2tHCJSmVK+KcLw\nJek8whSNN25YB5xTWBvnYRvqafddbDqqkaRwkUGlEvsjjwnx3Ev7wgLrtcApX2kK\nvYrSENR2WFi7kIXKxOCbNLMtizI42lVmk8qD8k0qyQKBgQD+uL/C5y89LveX+qNh\nT9vNf3bad/J8JB4RusKBRqvbj+fx9VjGFqAE/61nftx2qzvVbxUB7O5eYbo/IOZ0\nNx11AgezkrwnMxLHnGcAV5bm6dc1KenBmNsT17ieVLFmakor3Y+uqlqa6AzS7Nvv\nLJdhRQE9DRgxH8IyxE/4Yv089QKBgQDoKMzvHjl1jsyTgWzABjxJUmZpftTJWD32\nax6B0YQVMgXRw4DpSuizy4eIQ5dQzoOOSn3ptB+WhGcLdplN0RKaIUrG5pb94mKY\nl1iWWYY0JgnZrLZRlVCU9QSbItNFuXOgyBilRNnd/4bM0RIsWUVAh2F6+RfV7KaO\nBnFrdG1iAwKBgC7wgr369lSTDCNgl6z+F2ZGH/PgeLnxuezQerlx+YeLfFW1Pbe7\nQZ/KMf1Jfn0Lzwnk330sWTzhS34IpQ8zfOUsC5lG6A9dNQ+9valteqxw2Sy93YrE\njVKZxTbAKrTRz3tH6Atq7yluq7ZO2bS4hejx22mRuSyMSrAqRwZhz4LlAoGBALuD\nqPNCUFq5eldx5Cq9XphCa+GtxaTTF0kfgEaOucZ83uJsrITCMm52aAQRw/hKtUuK\ns2S+Tf0++BP0OSL3SbUgSsuYmEB8R/LD25Okv5cDWlVtAN2oYqvNs2iymj7o6ZHl\nzLJAiFCdAX4RKEKV4C+cqYmwBCaPvH9eiOGn6mmNAoGBAL1d5tVK0cWDO85f2fO4\nsfctpWD25l0aAe8+m+ArrHXa5q1l87c/iL/RGA4A23m56zkYF49kB/+WTnvN6AK4\nGutQiChnxcE+uuFOT2z6BMEHNmEc31hERrLn716/Mq/mAOiFtkk6aLQgJlDZF/dZ\nMHGWubZOSuk90cOLFKA7eXQi\n-----END PRIVATE KEY-----\n",
+  client_email:
+    "firebase-adminsdk-fbsvc@blog-project-77264.iam.gserviceaccount.com",
+  client_id: "106682706129209349511",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40blog-project-77264.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com",
+};
