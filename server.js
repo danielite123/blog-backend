1efe9a3ffca20d7db51d3c16ab408eb332b76acc
danielite123 <danielite100@gmail.com>
2025-04-08 14:35:46 +0100

    update password

--- server.js
+++ server.js
@@ -213,6 +213,69 @@ server.post("/google-auth", async (req, res) => {
   }
 });
 
+server.post("/change-password", verifyJWT, (req, res) => {
+  let { currentPassword, newPassword } = req.body;
+
+  if (
+    !passwordRegex.test(currentPassword) ||
+    !passwordRegex.test(newPassword)
+  ) {
+    return res.status(403).json({
+      error:
+        "Password should be 6 to 20 character long with a numeric, 1 lowercase and 1 uppercase letter",
+    });
+  }
+
+  User.findOne({ _id: req.user })
+    .then((user) => {
+      if (user.google_auth) {
+        return res.status(403).json({
+          error:
+            "You can't change accounts password  beacause you login through google",
+        });
+      }
+
+      bcrypt.compare(
+        currentPassword,
+        user.personal_info.password,
+        (err, result) => {
+          if (err) {
+            return res.status(500).json({
+              error:
+                "Some error occured while changing the password, please try again later",
+            });
+          }
+
+          if (!result) {
+            return res
+              .status(403)
+              .json({ error: "Incorrect current password" });
+          }
+
+          bcrypt.hash(newPassword, 10, (err, hashed_password) => {
+            User.findOneAndUpdate(
+              { _id: req.user },
+              { "personal_info.password": hashed_password }
+            )
+              .then((u) => {
+                return res.status(200).json({ status: "password changed" });
+              })
+              .catch((err) => {
+                return res.status(500).json({
+                  error:
+                    "Some error occured while saving new pasword, please try again later",
+                });
+              });
+          });
+        }
+      );
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "User not found" });
+    });
+});
+
 server.post("/upload", upload.single("image"), async (req, res) => {
   try {
     if (!req.file) {
